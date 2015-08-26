a96087a4f6ef5e7821d25a848fc30bb5f639fcfa
philmphoenix <jpholzmann@gmail.com>
2015-08-26 13:49:57 -0600

    all crud elements tested

--- spec/models/task_manager_spec.rb
+++ spec/models/task_manager_spec.rb
@@ -39,4 +39,74 @@ RSpec.describe TaskManager do
       expect(expected.map { |task| task.id }).to eq([1, 2])
     end
   end
+
+  describe "#find" do
+    it "returns the object being searched for" do
+      attributes = { :id => 1,
+                     :title => "sweet title",
+                     :description => "good description"
+      }
+      TaskManager.create(attributes)
+
+      moar_attributes = { :id => 2,
+                     :title => "other title",
+                     :description => "better description"
+      }
+      TaskManager.create(moar_attributes)
+
+      expected = TaskManager.find(2)
+
+      expect(expected.title).to eq("other title")
+      expect(expected.description).to eq("better description")
+    end
+  end
+
+  describe "#update" do
+    it "changes the values of the object" do
+      attributes = { :id => 1,
+                     :title => "sweet title",
+                     :description => "good description"
+      }
+      TaskManager.create(attributes)
+
+      moar_attributes = { :id => 2,
+                     :title => "other title",
+                     :description => "better description"
+      }
+      TaskManager.create(moar_attributes)
+
+      even_moar_attributes = { :title => "moar different title",
+                     :description => "moar different description"
+      }
+      TaskManager.update(2, even_moar_attributes)
+
+      expected = TaskManager.find(2)
+
+      expect(expected.title).to eq("moar different title")
+      expect(expected.description).to eq("moar different description")
+    end
+  end
+
+  describe "#destroy" do
+    it "destroys the given task" do
+      attributes = { :id => 1,
+                     :title => "sweet title",
+                     :description => "good description"
+      }
+      TaskManager.create(attributes)
+
+      moar_attributes = { :id => 2,
+                     :title => "other title",
+                     :description => "better description"
+      }
+      TaskManager.create(moar_attributes)
+
+      expect(TaskManager.all.count).to eq(2)
+
+      TaskManager.delete(2)
+
+      expect(TaskManager.all.count).to eq(1)
+      expect(TaskManager.all.last.title).to eq("sweet title")
+    end
+  end
 end
